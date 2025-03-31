f8ac543376aa3572253d40c260cd0e7ae7cc96d6
SergioCaMi <sergiocami84@gmail.com>
2025-03-31 20:27:16 +0200

    fin ejerccio

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // ¡Buena suerte!
+const bill = document.getElementById("bill");
+const tip = document.getElementById("tip");
+const total = document.getElementById("total");
+const button = document.getElementById("calculate");
+
+button.addEventListener("click", ()=>{
+    total.textContent = bill.value*(1+(+tip.value/100)) + "€";
+});
